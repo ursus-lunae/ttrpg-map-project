a92a688c70dbffc25f2d9bcf22be69b5e093022d
Dimitris Kanakaris <dimitriskanakaris@gmail.com>
2026-01-20 13:18:38 +0200

    Create locations.js

---
+++ locations.js
@@ -0,0 +1,15 @@
+// locations.js
+const poiData = [
+    {
+        name: "Haven",
+        coords: [-400, 600],
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce congue tellus eu lacinia tristique.",
+        type: "Settlement"
+    },
+    {
+        name: "Ruins of the Sun-Worshippers",
+        coords: [-1200, 1400],
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce congue tellus eu lacinia tristique.",
+        type: "Ruins"
+    }
+];
