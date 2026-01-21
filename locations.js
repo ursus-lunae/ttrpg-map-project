e7cc37e4bb98f12de12a0a204369753cbc8031fe
Dimitris Kanakaris <dimitriskanakaris@gmail.com>
2026-01-21 03:04:26 +0200

    Add files via upload

--- locations.js
+++ locations.js
@@ -3,13 +3,55 @@ const poiData = [
     {
         name: "Haven",
         coords: [-1500, -200],
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce congue tellus eu lacinia tristique.",
-        type: "Settlement"
+        description: "Haven is a home for the homeless. Far north, next to the vast, remote swampland that is the Fingerbog, this small community is slowly growing and claiming the surrounding territories. This land is hostile and unforgiving, and Nature is a harsh enemy, but all dangers pale in comparison to the settlers’ dream: freedom from the scuffles of old, independence, and a new home that hopefully lacks the faults of each settler’s past dwellings.",
+        type: ["Settlement", "Finger Menhir"]
     },
     {
         name: "Sun Temple Ruins",
-        coords: [-140, -1240],
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce congue tellus eu lacinia tristique.",
-        type: "Ruins"
+        coords: [-800, -1200],
+        description: "Ancient sun-worshipper ruins half-drowned by the swamp. Charred stone, fire-marked murals, and lingering heat hint at rituals meant to channel the sun, devotion, and something far older than Solstice.",
+        type: ["Ruins", "Finger Menhir"]
+    },
+    {
+        name: "Weird Endless Bush Fence",
+        coords: [-1200, -1400],
+        description: "A vast, unnatural barrier of dense, thorned growth stretching into Nubelwod farther than sight allows.",
+        type: "Landmark"
+    },
+    {
+        name: "Wild Hunter Worshippers",
+        coords: [-2900, 1900],
+        description: "A mobile congregation of traditionalist fauns deep in the forest, led by a faun named Attiva, bound by blood-rites, sacrifice, and communal reverence for the Wild Hunter.",
+        type: "Settlement?"
+    },
+    {
+        name: "Solstice",
+        coords: [-900, 1300],
+        description: "A Sunstone-lit settlement built on sun worship and fractured by truth. Troubled relationship with Haven.",
+        type: "Settlement"
+    },
+    {
+        name: "Ilzandaira",
+        coords: [-1000, 250],
+        description: "A willow creature named Ilzandaira lives in the swamp, a couple of days' travel from Haven. <b>Trickledowns</b>—flowers that can be used for medicine—grow in her grove, and she is willing to share in return for our company. She apparently finds us entertaining…",
+        type: "POI"
+    },
+    {
+        name: "Finger Monolith",
+        coords: [600, 1400],
+        description: "One of the towering stone fingers rising from the land, carved and shaped long before living memory. Troll legends speak of networks, messages, or paths once etched into such monoliths. Knowledge now mostly lost.",
+        type: "Landmark"
+    },
+    {
+        name: "Finger Monolith",
+        coords: [600, -1350],
+        description: "One of the towering stone fingers rising from the land, carved and shaped long before living memory. Troll legends speak of networks, messages, or paths once etched into such monoliths. Knowledge now mostly lost.",
+        type: "Landmark"
+    },
+    {
+        name: "Stiltwalker Village",
+        coords: [100, -400],
+        description: "A settlement of blind inhabitants built high above the swamp on wooden stilts. Mostly unlit and guided by sound, touch, and bioluminescent growths, the village embodies adaptation rather than defiance of the Fingerbog.",
+        type: "Settlement"
     }
 ];
